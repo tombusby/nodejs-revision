67876663a759e96fab99bc7ca128a2b4914e9804
Tom Busby <tom@busby.ninja>
2018-08-19 23:24:57 +0200

    Add HelloWorld app

---
+++ 01-helloworld/app.js
@@ -0,0 +1,17 @@
+
+"use strict";
+
+const http = require('http');
+
+const hostname = '192.168.39.12';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
