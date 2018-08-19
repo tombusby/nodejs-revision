aefe9bd20e379c033f205a64ed21e096131c0263
Tom Busby <tom@busby.ninja>
2018-08-19 23:27:14 +0200

    Add node-gyp to bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -7,3 +7,6 @@ sudo apt-get install nodejs npm mongodb -y
 # sudo su -c "sed -e 's/127\.0\.0\.1/0.0.0.0/' /etc/mongodb.conf > mongodb.conf \
 # 	&& sudo mv mongodb.conf /etc/mongodb.conf" -s /bin/sh root
 # sudo systemctl restart mongodb.service
+
+# Allow the installation of native extensions
+sudo npm install -g node-gyp
